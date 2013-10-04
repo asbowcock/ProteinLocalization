2797c12ac03220fdfc4ccea1ee7c8e7033e99614
asbowcock <alec.bowcock@pacificu.edu>
2013-10-03 21:06:09 -0700

    commented normalizeContourValues and getContourValues; tested getUniqueContourValues

--- kdeAnalysis/normalizeContourValues.m
+++ kdeAnalysis/normalizeContourValues.m
@@ -1,3 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Method:      normalizeContourValues
+%
+% Description: Normalizes the passed-in contour values to values between 0
+%              and 1, inclusive.
+%
+% Parameters:  contourValues      - values for each contour line
+%
+% Returned:    The normalized contour values
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [normContourValues] = normalizeContourValues (contourValues)
 
 normContourValues = contourValues;
