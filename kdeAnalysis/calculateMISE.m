eed2c20f56b65806401514f134a1e91e9c661334
asbowcock <alec.bowcock@pacificu.edu>
2014-01-27 11:03:42 -0800

    moved unused function files to another folder; updated documentation

--- kdeAnalysis/calculateMISE.m
+++
@@ -1,7 +0,0 @@
-function [mise] = calculateMISE (currentPDE, prevPDE, xBinSize, yBinSize)
-
-mise = mean (mean (currentPDE)) * trapz (trapz ((currentPDE - prevPDE).^2)) ...
-       * xBinSize * yBinSize;
-
-end
-
--- kdeAnalysis/calculateRMSE.m
+++
@@ -1,9 +0,0 @@
-function [rmse, sumPDEDiffSquared] = calculateRMSE (currentPDE, prevPDE, qdCount, sumPDEDiffSquared)
-
-sumPDEDiffSquared = sumPDEDiffSquared + sqrt (sum (sum ((currentPDE - prevPDE).^2))); 
-
-rmse = 1 / qdCount * sumPDEDiffSquared;
-
-end
-
-
